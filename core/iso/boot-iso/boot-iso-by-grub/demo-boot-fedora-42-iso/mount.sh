d6889b040d03db47a94f76699455763e4a3bac35
samwhelp <samwhelp@users.noreply.github.com>
2025-04-17 11:26:36 +0800

    fix

---
+++ core/iso/boot-iso/boot-iso-by-grub/demo-boot-fedora-42-iso/mount.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+
+
+mkdir -p mnt
+
+
+
+
+sudo mount -o loop Fedora-Workstation-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-KDE-Desktop-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-Xfce-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-LXQt-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-LXDE-Live-x86_64-42-1.1.iso mnt
+
+#sudo mount -o loop Fedora-Budgie-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-MATE_Compiz-Live-x86_64-42-1.1.iso mnt
+
+#sudo mount -o loop Fedora-Cinnamon-Live-x86_64-42-1.1.iso mnt
+
+#sudo mount -o loop Fedora-COSMIC-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-i3-Live-x86_64-42-1.1.iso mnt
+
+#sudo mount -o loop Fedora-Sway-Live-x86_64-42-1.1.iso mnt
+
+#sudo mount -o loop Fedora-MiracleWM-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-SoaS-Live-42-1.1.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-KDE-Mobile-Live-42-1.1.x86_64.iso mnt
