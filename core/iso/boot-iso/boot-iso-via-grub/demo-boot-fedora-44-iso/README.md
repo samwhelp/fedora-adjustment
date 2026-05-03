11c158b326b9bfcf747d3d550a5ee46299e5b633
samwhelp <samwhelp@users.noreply.github.com>
2026-05-03 19:50:40 +0800

    fix

---
+++ core/iso/boot-iso/boot-iso-via-grub/demo-boot-fedora-44-iso/README.md
@@ -0,0 +1,4 @@
+
+
+# demo-boot-fedora-44-iso
+
---
+++ core/iso/boot-iso/boot-iso-via-grub/demo-boot-fedora-44-iso/mount.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+
+
+mkdir -p mnt
+
+
+
+
+sudo mount -o loop Fedora-Workstation-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-KDE-Desktop-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-Xfce-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-LXQt-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-LXDE-Live-x86_64-44-1.7.iso mnt
+
+#sudo mount -o loop Fedora-Budgie-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-MATE_Compiz-Live-x86_64-44-1.7.iso mnt
+
+#sudo mount -o loop Fedora-Cinnamon-Live-x86_64-44-1.7.iso mnt
+
+#sudo mount -o loop Fedora-COSMIC-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-i3-Live-x86_64-44-1.7.iso mnt
+
+#sudo mount -o loop Fedora-Sway-Live-x86_64-44-1.7.iso mnt
+
+#sudo mount -o loop Fedora-MiracleWM-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-SoaS-Live-44-1.7.x86_64.iso mnt
+
+#sudo mount -o loop Fedora-KDE-Mobile-Live-44-1.7.x86_64.iso mnt
